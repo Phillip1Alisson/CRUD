dd4686f33ec1bbc63a7b62ac72d13675ae0408cc
Phillip <phill.volpi@gmail.com>
2017-03-23 14:19:05 -0300

    Create file Item.php

---
+++ src/Model/Item.php
@@ -0,0 +1,14 @@
+<?php
+	namespace Model;
+
+	class Item{
+		private $idItem;
+		private $nameItem;
+
+		fucntion Item($name, $id){
+			$this->idItem = $id;
+			$this->nameItem = $name;
+		}
+
+	}
+?>
