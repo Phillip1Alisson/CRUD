144ae22371c0e9fce6dec0fd3c328b98cb9a0690
Phillip <phill.volpi@gmail.com>
2017-03-24 14:05:39 -0300

    Updates

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-	include_once('src/Model/User.class.php');
-	include_once('src/Model/Item.class.php');
-	include_once('src/DAO/CommandSQL.class.php');
-	include_once('src/DAO/UpdateInterface.class.php');
-	include_once('src/DAO/UserSQL.class.php');
-
-
-	$item[] = new Item(1, "Ferrari") ;
-	$user   = new User("Phillip", 1, "gnkscksdkfj@asjkkashdkdsa", "123456",$item);
-	$UserSQL = new UserSQL();
-	var_dump($UserSQL->insert($user));
-	print_r($user);
 	
 ?>
--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,6 +1,8 @@
 <?php
-class UserController{
-	public static $user;
+class Controller{
+	private static $user;
+	private static $ItemSQL = new ItemSQL;
+	public  static $Itens   = self::searchItens();
 	private static $userSQL = new UserSQL;
 
 	public static function loginUser(){
@@ -53,9 +55,36 @@ class UserController{
 
 	}
 
+		public static function createItem(){
+			$name_item = $__POST["name_item"];
+			self::$itemSQL->insert(new Item(NULL, $name));
+			$item = self::$itemSQL->select(new Item(NULL, $name));
+			$item->fetch_array(MYSQLI_ASSOC);
+			$itens[] = new Item($item["id_item"], $name);
 
+			echo 'inserção realizada com sucesso';
+		}
 
+		public static function deleteItem(){
+			$name = $__POST["name_item"];
 
+			foreach(self::$itens as $key = $value){
+				if($value->getName_item() == $name){
+					$desireList = new DesireListSQL;
+					$desireList->deleteItemOfDesireList($value);
+					self::$itemSQL->delete($value);
+				}
+
+			}
+		}
+
+		private static function searchItens(){
+			$item = self::$itemSQL->select(NULL);
+			while($item->fetch_array(MYSQLI_ASSOC)){
+				$itns[] = new Item($item["id_item"], $item["name_item"]);
+			}
+			return $itns;
+		}
 }
 
 
